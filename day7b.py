7f00f449753de8cf7c41da455b7388061ab364f2
paulderaadt <paulderaadt@gmail.com>
2023-12-16 23:21:03 +0100

    finally fixed day 7 part 2

--- day7b.py
+++ day7b.py
@@ -4,9 +4,8 @@ from collections import Counter
 
 from utils.filereaders import Linereader
 
-data = Linereader("inputs/day7.txt").parse()
-data = [line.split(" ") for line in data]
-faces = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+faces = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2",
+         "J"]
 values = list(range(len(faces)))[::-1]
 CARDVALS = dict(zip(faces, values))
 
@@ -20,51 +19,50 @@ class Hand:
     @property
     def hand_score(self):
         score = 0
-        highest = self._highest_card()
-        if self.cards == "JJJJJ":
-            return 5**2
         if "J" not in self.cards:
             for item in self.cardset.values():
                 score += item**2
             return score
-        for item in self.cardset.values():
-            if item != "J" and item == highest:
-                score += (self.cardset["J"] + self.cardset[highest])**2
-            else:
+        if self.cards == list("JJJJJ"):
+            for item in self.cardset.values():
                 score += item**2
-        print(self, (self.cardset["J"] + self.cardset[highest])**2)
+            return score
+        kopie = self.cardset.copy()
+        jokers = kopie.pop("J")
+        highest_values = [kv for kv in kopie.items() if kv[1] == max(
+            kopie.values())]
+        hoogste_set = max(highest_values, key=lambda x: CARDVALS[x[0]])[0]
+        kopie[hoogste_set] += jokers
+        for item in kopie.values():
+            score += item ** 2
         return score
-
-    def _highest_card(self):
-        highest_values = [kv for kv in self.cardset.items() if kv[1] == max(
-            self.cardset.values())]
-        return max(highest_values, key=lambda x: CARDVALS[x[0]])[0]
-
-    def _lowest_card(self):
-        lowest_values = [kv for kv in self.cardset.items() if kv[1] == min(
-            self.cardset.values()) and kv[0] != "J"]
-        return min(lowest_values, key=lambda x: CARDVALS[x[0]])[0]
 
     def __repr__(self):
         return "".join(self.cards)
 
-    def __gt__(self, other):
+    def __lt__(self, other):
         if self.hand_score == other.hand_score:
             for i in range(len(self.cards)):
                 if self.cards[i] == other.cards[i]:
                     continue
                 else:
-                    return CARDVALS[self.cards[i]] > CARDVALS[other.cards[i]]
-        return self.hand_score > other.hand_score
+                    # print(self.cards[i], CARDVALS[self.cards[i]], CARDVALS[
+                    #     other.cards[i]], other.cards[i])
+                    return CARDVALS[self.cards[i]] < CARDVALS[other.cards[i]]
+        return self.hand_score < other.hand_score
 
 
+data = Linereader("inputs/day7.txt").parse()
+data = [line.split(" ") for line in data]
+hands = []
 for line in data:
     hands.append(Hand(line[0], line[1]))
 
+# scores for 1 2 3 4 or 5 of a kind [1, 4, 9, 16, 25]
 hands.sort()
 winnings = 0
 rank = 1
 for hand in hands:
     winnings += hand.bet * rank
     rank += 1
-    print('score', hand, hand.hand_score)
+print(winnings)
